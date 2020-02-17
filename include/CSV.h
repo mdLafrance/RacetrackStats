3729e49302523a05dca5dc5d1a1932272ca5f5bc
mdlafrance <maxtorious@gmail.com>
2020-02-17 15:02:33 -0500

    Fixed name of CSV, and now has all functionality

---
+++ include/CSV.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include <map>
+
+#include <Utils.h>
+
+class CSV {
+    int numberOfLines;
+    int numberOfFields;
+
+    char** data;
+
+    std::map<std::string, int> dataOffsets;
+    std::map<std::string, std::string> dataTypes;
+
+public:
+    static std::pair<std::string, std::string> splitNameAndType(const std::string& line);
+
+    std::string getData(const std::string& type, const int& line) const;
+    std::string getData(const int& index, const int& line) const;
+
+    bool hasData(const std::string& type);
+
+    CSV(const std::string& targt);
+    ~CSV();
+};
--- src/CSV.cpp
+++ src/CSV.cpp
@@ -1,6 +1,6 @@
-#include <CVS.h>
+#include <CSV.h>
 
-CVS::CVS(const std::string& target) {
+CSV::CSV(const std::string& target) {
     Utils::StopWatch sw;
 
     FILE* f = fopen(target.c_str(), "r");
@@ -14,7 +14,7 @@ CVS::CVS(const std::string& target) {
         return;
     }
 
-    std::cout << "Loading CVS file: " << target << ' ';
+    std::cout << "Loading CSV file: " << target << ' ';
 
     sw.start();
 
@@ -54,11 +54,14 @@ CVS::CVS(const std::string& target) {
 
     line_s = line_s.substr(0, line_s.size() - 1); // Chop \n
 
-    std::vector<std::string> types = Utils::split(line_s, ',');
+    std::string dataName;
+    std::string dataType;
 
-    for (std::string t : types) {
-        // std::cout << "Registered <" << t << "> at " << i << std::endl;
-        this->dataOffsets[t] = i++;
+    for (std::string t : Utils::split(line_s, ',')) {
+        auto dataAndType = CSV::splitNameAndType(t);
+
+        this->dataOffsets[dataAndType.first] = i++;
+        this->dataTypes[dataAndType.first] = dataAndType.second;
     }
 
     i = 0;
@@ -113,7 +116,7 @@ nextword:
     std::cout << " (" << sw.lap_s() << ")" << std::endl;
 }
 
-CVS::~CVS(){
+CSV::~CSV(){
     for (int i = 0; i < (this->numberOfFields * this->numberOfLines); i++) {
 		delete *(this->data + i);
     }
@@ -121,8 +124,49 @@ CVS::~CVS(){
     delete[] this->data;
 }
 
-char* CVS::getData(char* type, const int& line) {
-    int offset = this->dataOffsets[type];
+std::string CSV::getData(const std::string& type, const int& line) const {
+    if (this->dataOffsets.count(type) == 1) {
+		int offset = this->dataOffsets.at(type);
+		return *(this->data + (line * this->numberOfFields) + offset);
+    }
+    else {
+        std::cerr << "ERROR: Couldn't access data field <" << type << "> because it is not present in the csv." << std::endl;
+		return "";
+    }
+}
 
-    return *(this->data + (line * this->numberOfFields) + offset);
+std::string CSV::getData(const int& index, const int& line) const {
+    if (!(0 <= index <= this->numberOfFields)) {
+        throw std::out_of_range("Index is out of range.");
+    }
+
+    return *(this->data + (line * this->numberOfFields) + index);
+}
+
+bool CSV::hasData(const std::string& type) {
+    return this->dataTypes.count(type) == 1;
+}
+    
+std::pair<std::string, std::string> CSV::splitNameAndType(const std::string& line) {
+    if (line.find_first_of('[') == std::string::npos) {
+        return std::pair<std::string, std::string>(line, "");
+    }
+
+    const char* c = line.c_str();
+
+    int length = line.size();
+
+    int braceLocation;
+
+    for (int i = length-1;i >=0 ; i--){
+        if (c[i] == '['){
+            braceLocation = i;
+            break;
+        }
+    }
+
+    return std::pair<std::string, std::string>(
+        Utils::trimWhitespace(line.substr(0, braceLocation - 1)), 
+        Utils::trimWhitespace(line.substr(braceLocation, length))
+    );
 }
